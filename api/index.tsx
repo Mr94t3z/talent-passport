21b79246b501d4a7dc68f54a33e0d2eb251a13a0
Muhamad Taopik <mr.94t3z@gmail.com>
2024-11-10 20:04:45 +0700

    adding bg image

--- api/index.tsx
+++ api/index.tsx
@@ -105,7 +105,7 @@ app.image('/initial-image', (c) => {
     image: (
       <Box
           alignVertical="center"
-          backgroundImage="url(https://raw.githubusercontent.com/Mr94t3z/talent-passport/refs/heads/master/public/union.png)"
+          backgroundImage="url(https://raw.githubusercontent.com/Mr94t3z/talent-passport/refs/heads/master/public/bg-2.png)"
           backgroundColor="bg"
           justifyContent="center"
           paddingTop="52"
